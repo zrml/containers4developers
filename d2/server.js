fe735c54b0847716b77c44d5333dbd792e7a15ba
Erik Hemdal <ehemdal@intersystems.com>
2020-03-18 08:42:47 -0400

    Added --rm to run.sh; added newline to output of server.js to make display a little more readable. Tested under Docker for Windows.

--- d2/server.js
+++ d2/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello Wonderful Developers!");
+  response.end("\nHello Wonderful Developers!");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
